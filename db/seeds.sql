a2ec372d2af0d28091d0711d70d5233a30ffed36
Brittney Roy <brittroy1@gmail.com>
2020-06-26 20:59:27 -0400

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT into burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT into burgers (burger_name) VALUES ('Veggie Burger');
+INSERT into burgers (burger_name) VALUES ('Chicken Burger');
